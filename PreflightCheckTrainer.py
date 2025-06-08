f52fdd682b71acd051380ad28d51d2b804f2bb04
Ikshul Bethur <ikshulbethur@WMD.local>
2025-06-08 12:18:38 -0700

    *Working data creationV1*

--- PreflightCheckTrainer.py
+++ PreflightCheckTrainer.py
@@ -1,17 +1,22 @@
 import torch
-import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
 from pathlib import Path
 import ConvertSTLtoVoxel as conv
 
 resolution = 100  # Number of slices/images per file
+convert = False  # Set to True if you need to convert STL files to PNG
+if convert:
+    # Get current script directory
+    current_dir = Path(__file__).resolve().parent
 
-# Get current script directory
-current_dir = Path(__file__).resolve().parent
+    CADmodel = str(current_dir / 'PreflightCheckTrainingData/CADmodel')
+    MESHmodel = str(current_dir / 'PreflightCheckTrainingData/MESHmodel')
 
-CADmodel = str(current_dir / 'PreflightCheckTrainingData/CADmodel')
-MESHmodel = str(current_dir / 'PreflightCheckTrainingData/MESHmodel')
-
-# Process CADmodel and MESHmodel directories
-conv.process_stl_files(CADmodel, resolution)
-conv.process_stl_files(MESHmodel, resolution)
+    # Process CADmodel and MESHmodel directories
+    conv.process_stl_files(CADmodel, resolution)
+    conv.process_stl_files(MESHmodel, resolution)
 
