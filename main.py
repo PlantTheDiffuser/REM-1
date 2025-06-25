1c61aab5dc46261294f88e223c294cab167863e1
Ikshul Bethur <ikshulbethur@WMD.local>
2025-06-25 12:46:42 -0700

    Added PreflightCheck as a function

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ from pathlib import Path
 import ConvertSTLtoVoxel as conv
 import PreflightCheckTrainer
 
-inputfile = 'test.STL'
-inputfilename = inputfile.split('.')[0]
-
+inputfile = 'model-4.STL'
 current_dir = Path(__file__).resolve().parent
-input = str(current_dir / inputfile)
-conv.PreprocessSingleFile(input, resolution=150)
 
+def RunPreflightCheck(inputfile):
+    inputfilename = inputfile.split('.')[0]
+    input = str(current_dir / inputfile)
+    conv.PreprocessSingleFile(input, resolution=150)
+    input = inputfilename + '.png'
+    input = str(current_dir / input)
+    output = PreflightCheckTrainer.predict_image(input)
+    #print("Output:", output)
 
-input = inputfilename + '.png'
-input = str(current_dir / input)
-output = PreflightCheckTrainer.predict_image(input)
-print("Output:", output)
+    # Clean up temporary files
+    input = Path(input)
+    if input.exists():
+        input.unlink()
+        #print(f"🧹 Cleaned up temporary file: {input}")
+    return output
 
-# Clean up temporary files
-input = Path(input)
-if input.exists():
-    input.unlink()
-    #print(f"🧹 Cleaned up temporary file: {input}")
+out = RunPreflightCheck(inputfile)
+print(out)
